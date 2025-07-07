8064f4605a552b264871a53b4442df64de820231
M9-BIT <mehuliyamathur@gmail.com>
2025-07-07 17:19:53 +0530

    text

---
+++ functions.py
@@ -0,0 +1,15 @@
+#todos = []
+def read_todo(filename="todo.txt"):
+    with open(filename, "r") as file1:
+       my_todos = file1.readlines()
+       return my_todos
+
+
+def write_todo(todo_list,filename="todo.txt",):
+    with open(filename, "w") as file:
+        file.writelines(todo_list)
+
+print(type(__name__))
+if __name__ == "__main__":
+    print("this is my todo library")
+    print(read_todo())
